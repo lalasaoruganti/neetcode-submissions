0c6a771a356b01a830ef10d4b834e786ecda423f
lalasaoruganti <53698241+lalasaoruganti@users.noreply.github.com>
2026-08-18 13:41:14 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,14 @@
+variable = 4
+print(type(variable))
+variable = 4.6
+
+print(type(variable))
+variable = True
+
+print(type(variable))
+variable = "Hrllo"
+
+print(type(variable))
+variable = [2,4,6]
+
+print(type(variable))
